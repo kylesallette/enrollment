9a6072019c46e87c6162a0943a76d77ec3bd4d5a
kylesallette <kylesallette@gmail.com>
2018-03-27 13:26:12 -0600

    first test passing

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,12 @@
+class StudentsController < ApplicationController
+
+
+
+  def show
+    @student = Student.find(params[:id])
+  end 
+
+
+
+
+end
--- spec/features/user_visits_students_show_and_sees_students_name_spec.rb
+++ spec/features/user_visits_students_show_and_sees_students_name_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
   describe "user visits student show page"  do
     context "visit[student_path]"  do
       it "they see the name of the student"  do
-    student = Student.create(name: "kyle")
+      student = Student.create!(name: "kyle")
 
-    visit student_path
+      visit student_path(student.id)
 
-    expect(current_path).to eq(student_path)
+
     expect(page).to have_content(student.name)
   end
 end
-end 
+end
